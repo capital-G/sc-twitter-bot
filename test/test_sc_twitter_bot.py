a713c7fb420045a8f0bf5dcf9eadf143efc9c11f
Dennis Scheiba <dennis.scheiba@gmx.de>
2020-10-21 01:31:41 +0200

    add additional url test

--- test/test_sc_twitter_bot.py
+++ test/test_sc_twitter_bot.py
@@ -75,3 +75,19 @@ class TwitterBotTestCase(TestCase):
             self.twitter_bot._filter_out_synth_def(tweet),
             'SinOsc.ar'
         )
+
+    def test_real_life_example(self):
+        tweet = MagicMock(
+            text='@SC2Sbot  { https://t.co/355mI9rrLK(https://t.co/YeyelSXZsz(200, 0.1), https://t.co/eDJ2jx2I4u(… https://t.co/P8VnqUvTTd',
+            entities={
+                'urls': [
+                    {'url': 'https://t.co/355mI9rrLK', 'expanded_url': 'http://RLPF.ar', 'display_url': 'RLPF.ar', 'indices': [12, 35]},
+                    {'url': 'https://t.co/YeyelSXZsz', 'expanded_url': 'http://Saw.ar', 'display_url': 'Saw.ar', 'indices': [36, 59]},
+                    {'url': 'https://t.co/eDJ2jx2I4u', 'expanded_url': 'http://SinOsc.ar', 'display_url': 'SinOsc.ar', 'indices': [71, 94]},
+                    {'url': 'https://t.co/P8VnqUvTTd', 'expanded_url': 'https://twitter.com/i/web/status/1318694051760275456', 'display_url': 'twitter.com/i/web/status/1…', 'indices': [97, 120]},
+            ]}
+        )
+        self.assertEqual(
+            self.twitter_bot._filter_out_synth_def(tweet),
+            '{ http://RLPF.ar(http://Saw.ar(200, 0.1), http://SinOsc.ar(http://XLine.kr(0.7, 300, 20), 0, 3600, 4000), 0.2) }'
+        )
